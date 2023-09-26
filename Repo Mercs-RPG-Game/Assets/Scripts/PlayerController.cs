9c4bda564100fc29873178e4d361dc4d64c55a0f
Vermalreik <31960538+vermalreik@users.noreply.github.com>
2023-09-27 00:44:18 +0200

    Add Part 01
    :D From the youtube tutorial series "Make A Game Like Pokemon in Unity" #1

---
+++ Repo Mercs-RPG-Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed; //5
+
+    private bool isMoving;
+    private Vector2 input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal"); // Raw para k el input sea siempre 1 o -1
+            input.y = Input.GetAxisRaw("Vertical");
+
+            // remove diagonal movement
+            if (input.x != 0) input.y = 0;
+
+            if (input != Vector2.zero)
+            {
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                StartCoroutine(Move(targetPos));
+            }
+        }
+    }
+
+    IEnumerator Move(Vector3 targetPos) // IEnumerator is used to do something over a period of time
+    {
+        isMoving = true;
+
+        // check if the current position is greater than a very small value Mathf.Epsilon
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            // if it is, then we will use the MoveTowards function to move the player towards the target position by a very small amount
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null; // this stops the execution of the coroutine and resume it in the next Update function
+        }
+        transform.position = targetPos; // set the player current position to the target position
+        isMoving = false;
+    }
+}
