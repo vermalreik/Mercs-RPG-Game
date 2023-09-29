0bc5436413b492d2b1571b996ad56a1fdf74c6ec
Vermalreik <31960538+vermalreik@users.noreply.github.com>
2023-09-29 23:49:41 +0200

    Part 05
    Creating Pokemons Using Scriptable Objects

---
+++ Repo Mercs-RPG-Game/Assets/Pokemons/Pokemon.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pokemon // This is going to be plain C#, thats why we dont inherit from MonoBeahaviour
+{
+    PokemonBase _base;
+    int level;
+
+    public Pokemon(PokemonBase pBase, int pLevel)
+    {
+        _base = pBase;
+        level = pLevel;
+    }
+
+    public int Attack
+    {
+        get { return Mathf.FloorToInt(_base.Attack * level / 100f) + 5; }
+    }
+
+    public int Defense
+    {
+        get { return Mathf.FloorToInt(_base.Defense * level / 100f) + 5; }
+    }
+
+    public int SpAttack
+    {
+        get { return Mathf.FloorToInt(_base.SpAttack * level / 100f) + 5; }
+    }
+
+    public int SpDefense
+    {
+        get { return Mathf.FloorToInt(_base.SpDefense * level / 100f) + 5; }
+    }
+
+    public int Speed
+    {
+        get { return Mathf.FloorToInt(_base.Speed * level / 100f) + 5; }
+    }
+
+    public int MaxHp
+    {
+        get
+        {
+            return Mathf.FloorToInt(_base.Speed * level / 100f) + 10;
+        }
+    }
+}
