dd117bf2d7f02714771bd338c45a80f147debcb6
Vermalreik <31960538+vermalreik@users.noreply.github.com>
2023-11-17 21:31:30 +0100

    Part 80
    Shops 2 - Money

--- Repo Mercs-RPG-Game/Assets/Scripts/Items/ShopController.cs
+++ Repo Mercs-RPG-Game/Assets/Scripts/Items/ShopController.cs
@@ -9,6 +9,7 @@ public enum ShopState { Menu, Buying, Selling, Busy }
 public class ShopController : MonoBehaviour
 {
     [SerializeField] InventoryUI inventoryUI;
+    [SerializeField] WalletUI walletUI;
 
     public event Action OnStart;
     public event Action OnFinish;
@@ -86,6 +87,8 @@ public class ShopController : MonoBehaviour
             yield break;
         }
 
+        walletUI.Show();
+
         float sellingPrice = Mathf.Round(item.Price / 2);
 
         int selectedChoice = 0;
@@ -98,10 +101,12 @@ public class ShopController : MonoBehaviour
         {
             // Yes
             inventory.RemoveItem(item);
-            // TODO: Add selling price to player's wallet
+            Wallet.i.AddMoney(sellingPrice);
             yield return DialogManager.Instance.ShowDialogText($"Turned over {item.Name} and received {sellingPrice}");
         }
-        
+
+        walletUI.Close();
+
         state = ShopState.Selling;
     }
 }
