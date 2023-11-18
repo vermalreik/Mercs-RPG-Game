b7ba1750368c663d5d68158c1760d4949e661d05
Vermalreik <31960538+vermalreik@users.noreply.github.com>
2023-11-18 21:16:29 +0100

    Part 84
    Completing Shop System

--- Repo Mercs-RPG-Game/Assets/Scripts/Items/ShopController.cs
+++ Repo Mercs-RPG-Game/Assets/Scripts/Items/ShopController.cs
@@ -8,6 +8,7 @@ public enum ShopState { Menu, Buying, Selling, Busy }
 
 public class ShopController : MonoBehaviour
 {
+    [SerializeField] Vector2 shopCameraOffset;
     [SerializeField] InventoryUI inventoryUI;
     [SerializeField] ShopUI shopUI;
     [SerializeField] WalletUI walletUI;
@@ -53,10 +54,12 @@ public class ShopController : MonoBehaviour
         if(selectedChoice == 0)
         {
             // Buy
-            state = ShopState.Buying;
+            yield return GameController.Instance.MoveCamera(shopCameraOffset);
             walletUI.Show();
             shopUI.Show(merchant.AvailableItems, (item) => StartCoroutine(BuyItem(item)),
-                OnBackFromBuying);
+                () => StartCoroutine(OnBackFromBuying()));
+
+            state = ShopState.Buying;
         }
         else if(selectedChoice == 1)
         {
@@ -178,8 +181,9 @@ public class ShopController : MonoBehaviour
         state = ShopState.Buying;
     }
 
-    void OnBackFromBuying()
+    IEnumerator OnBackFromBuying()
     {
+        yield return GameController.Instance.MoveCamera(-shopCameraOffset);
         shopUI.Close();
         walletUI.Close();
         StartCoroutine(StartMenuState());
