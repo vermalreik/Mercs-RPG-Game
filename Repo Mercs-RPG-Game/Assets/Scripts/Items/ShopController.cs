03e0ac97eccbf7a55e34617e5dd1c1aa44187cc2
Vermalreik <31960538+vermalreik@users.noreply.github.com>
2023-11-18 01:06:02 +0100

    Part 82
    Shops 4 - Shop UI

--- Repo Mercs-RPG-Game/Assets/Scripts/Items/ShopController.cs
+++ Repo Mercs-RPG-Game/Assets/Scripts/Items/ShopController.cs
@@ -9,6 +9,7 @@ public enum ShopState { Menu, Buying, Selling, Busy }
 public class ShopController : MonoBehaviour
 {
     [SerializeField] InventoryUI inventoryUI;
+    [SerializeField] ShopUI shopUI;
     [SerializeField] WalletUI walletUI;
     [SerializeField] CountSelectorUI countSelectorUI;
 
@@ -16,6 +17,8 @@ public class ShopController : MonoBehaviour
     public event Action OnFinish;
 
     ShopState state;
+
+    Merchant merchant;
 
     public static ShopController i { get; private set; }
     private void Awake()
@@ -31,6 +34,8 @@ public class ShopController : MonoBehaviour
     
     public IEnumerator StartTrading(Merchant merchant)
     {
+        this.merchant = merchant;
+
         OnStart?.Invoke();
         yield return StartMenuState();
     }
@@ -48,6 +53,9 @@ public class ShopController : MonoBehaviour
         if(selectedChoice == 0)
         {
             // Buy
+            state = ShopState.Buying;
+            walletUI.Show();
+            shopUI.Show(merchant.AvailableItems);
         }
         else if(selectedChoice == 1)
         {
@@ -68,6 +76,10 @@ public class ShopController : MonoBehaviour
         if(state == ShopState.Selling)
         {
             inventoryUI.HandleUpdate(OnBackFromSelling, (selectedItem) => StartCoroutine(SellItem(selectedItem)));
+        }
+        else if(state == ShopState.Buying)
+        {
+            shopUI.HandleUpdate();
         }
     }
 
