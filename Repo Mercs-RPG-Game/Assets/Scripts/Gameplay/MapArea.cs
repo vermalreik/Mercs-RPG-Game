14119b7f9bb9ff8c829cac92317f779132b6ad62
Vermalreik <31960538+vermalreik@users.noreply.github.com>
2023-11-20 19:02:02 +0100

    Part 89
    Improving Wild Encounters

--- Repo Mercs-RPG-Game/Assets/Scripts/Gameplay/MapArea.cs
+++ Repo Mercs-RPG-Game/Assets/Scripts/Gameplay/MapArea.cs
@@ -1,15 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class MapArea : MonoBehaviour
 {
-    [SerializeField] List<Pokemon> wildPokemons;
+    [SerializeField] List<PokemonEncounterRecord> wildPokemons;
+
+    private void Start()
+    {
+        int totalChance = 0;
+        foreach (var record in wildPokemons)
+        {
+            record.chanceLower = totalChance;
+            record.chanceUpper = totalChance + record.chancePercentage;
+
+            totalChance = totalChance + record.chancePercentage;
+        }
+    }
 
     public Pokemon GetRandomWildPokemon()
     {
-        var wildPokemon = wildPokemons[Random.Range(0, wildPokemons.Count)];
+        int randVal = Random.Range(1, 101);
+        var pokemonRecord = wildPokemons.First(p => randVal >= p.chanceLower && randVal <= p.chanceUpper);
+
+        var levelRange = pokemonRecord.levelRange;
+        int level = levelRange.y == 0 ? levelRange.x : Random.Range(levelRange.x, levelRange.y + 1); // + 1 because the second value is exclusive
+        
+        var wildPokemon = new Pokemon(pokemonRecord.pokemon, level);
         wildPokemon.Init();
         return wildPokemon;
     }
+}
+
+[System.Serializable]
+// When you specify the List to file Pokemon,
+//it's really important that the sum of all the chance percentages should be 100
+public class PokemonEncounterRecord // Data container
+{
+    public PokemonBase pokemon;
+    public Vector2Int levelRange;
+    public int chancePercentage;
+
+    public int chanceLower {get; set; }
+    public int chanceUpper {get; set; }
 }
